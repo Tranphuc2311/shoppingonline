3f0725f7666160f918c47951201761daae0756a6
unknown <tranphuc11114444@gmail.com>
2023-07-14 20:30:28 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.khjwurn.mongodb.net',
+  DB_USER: 'tranphuc11114444',
+  DB_PASS: '0972239991a',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'tranphuc11114444@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'nsmcfzrklafpjipr',
+  JWT_SECRET: 'TranPhuc',
+  JWT_EXPIRES: '365518000000', // in milliseconds
+};
+module.exports = MyConstants;
